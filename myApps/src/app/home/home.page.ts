21bd9554d2a5ad3c2ff126b05ed0c89532647a1d
Matheus Hora <matheus11hora@gmail.com>
2022-03-22 15:55:11 -0300

    Novos

--- myApps/src/app/home/home.page.ts
+++ myApps/src/app/home/home.page.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+
+  constructor(private navegacao: NavController) { }
+
+  ngOnInit() {
+  }
+
+  abrirBotoes(){
+    this.navegacao.navigateForward('botoes')
+  }
+
+  abrirLista(){
+    this.navegacao.navigateForward('lista')
+  }
 
   public titulo: String = "Meu primeiro App"
   public imagemRan: String = "https://source.unsplash.com/800x800"
